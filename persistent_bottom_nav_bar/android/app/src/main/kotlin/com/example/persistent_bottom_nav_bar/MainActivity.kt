d4feddc82cabe4c20963e4c3b548eb814ac0b57f
Dhina0508 <dhina0508@gmail.com>
2023-06-28 16:44:13 +0530

    28/6/23

--- persistent_bottom_nav_bar/android/app/src/main/kotlin/com/example/persistent_bottom_nav_bar/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.persistent_bottom_nav_bar
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ persistent_navbar/android/app/src/main/kotlin/com/example/persistent_navbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.persistent_navbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
